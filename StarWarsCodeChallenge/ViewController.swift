cffc253214f5a618a7f39a68c5fc434572070e98
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-05-31 17:55:33 -0600

    Initial Commit

---
+++ StarWarsCodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWarsCodeChallenge
+//
+//  Created by Taylor Bills on 5/31/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
